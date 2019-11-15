f3ad17ee2b6cf4fc51ff19c1e560a2a72b8c9c56
Heather Mason <wil06095@byui.edu>
2019-11-15 13:20:56 -0700

    barebones app

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ app.set('port', process.env.PORT || 5000)
    .set('view engine', 'ejs')
    .get('/', function(req, res){
       res.sendFile('form.html', { root: __dirname + "/public"});
-   });
+   })
+   .listen(app.get('port'), function() {
+    console.log('Listening on port: ' + app.get('port'));
+ });
